c0891e3018cfecbce80775a4fee77a71299e5362
Attekangasmaki <atte.kangasmaki@metropolia.fi>
2025-03-31 14:19:11 +0300

    Made some not null element null in db script

--- DB/db.sql
+++ DB/db.sql
@@ -5,12 +5,12 @@ CREATE TABLE users (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    birthday DATE NOT NULL,
+    first_name VARCHAR(100) NULL,
+    last_name VARCHAR(100) NULL,
+    birthday DATE NULL,
     height DECIMAL(5,2),
     weight DECIMAL(5,2),
-    gender ENUM('male', 'female', 'other') NOT NULL,
+    gender ENUM('male', 'female', 'other') NULL,
     user_level ENUM('user', 'professional', 'moderator') NOT NULL DEFAULT 'user'
 );
 
